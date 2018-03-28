abbe83d8f851adf741f758d5e97c400804e96721
Thibaut Patel <thibaut.patel@gmail.com>
2018-03-28 13:20:02 +0200

    Fixed an edge case where the index is set to a negative integer "higher" that the size of the array.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = class Cycled {
 	}
 
 	set index(i) {
-		this._index = (this.size + i) % this.size;
+		this._index = (this.size + (i % this.size)) % this.size;
 	}
 
 	step(steps) {
